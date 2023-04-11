47e44ea6e6ce883f0525ae0316c3b708edaab558
Darío Sánchez Echenique <dsanchez@iconsl.com>
2023-04-11 18:15:24 +0200

    yml

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace PFGWS.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecastacion")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
