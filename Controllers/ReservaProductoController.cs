e02b9475d49ff6a376f81699fc67d15bf3c4647d
Darío Sánchez Echenique <dsanchez@iconsl.com>
2023-06-22 17:33:21 +0200

    Fix

--- Controllers/ReservaProductoController.cs
+++ Controllers/ReservaProductoController.cs
@@ -62,10 +62,10 @@ namespace PFGWS.Controllers
             else
             {
                 Dictionary<int, int> newdic = nReservaProd.ToDictionary(keySelector: x=>x.producteid,elementSelector: x=>x.quantitat);
-                Dictionary<int, int> olddic = oldReservaProd.ToDictionary(keySelector: x => x.producteid, elementSelector: x => x.quantitat);
+                Dictionary<int, int> olddic = oldReservaProd.ToDictionary(keySelector: x => x.producteid, elementSelector: x => -x.quantitat);
                 Dictionary<int, int> result = (from e in newdic.Concat(olddic)
                                                group e by e.Key into g
-                                               select new { Name = g.Key, Count = g.Sum(kvp => -kvp.Value) }).ToDictionary(item => item.Name, item => item.Count);
+                                               select new { Name = g.Key, Count = g.Sum(kvp => kvp.Value) }).ToDictionary(item => item.Name, item => item.Count);
                 if(newidestado != oldidestado)
                 {
                     if((oldidestado ==5 || oldidestado == 6) && newidestado == 2)
